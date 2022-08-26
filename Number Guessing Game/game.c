ea1ea099a6982eee78124e90b955458951acd05a
Kumar Vishal Anand <vishalanand270@gmail.com>
2022-08-26 15:15:41 +0530

    Update game.c

--- Number Guessing Game/game.c
+++ Number Guessing Game/game.c
@@ -2,28 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Project_1- Number guessing game
+// Project- Number guessing game
 
 int main(){
     int number, guess, nguesses = 1;
     srand(time(0));
     number = rand() % 100+1 ; // Generates a random number between 1 and 100
-    // printf("The number is %d\n", number);
-
-        printf("Guess the number between 1 to 100\n");
+    
+    printf("Guess the number between 1 to 100\n");
     do
     {
         scanf("%d", &guess);
         if (guess > number)
         {
+            printf("Wrong!\n");
             printf("Lower number please\n");
         }
         else if (guess < number)
         {
+            printf("Wrong!\n");
             printf("Higher number please\n");
         }
         else
         {
+            printf("Correct!\n");
+            printf("The number is %d.\n", number);
             printf("You guessed in %d attempts\n", nguesses);
         }
         nguesses++;
